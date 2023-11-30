89f73c90f9efe7799306a4af82166cce80c24cfa
jly2k1 <jaimelobar@gmail.com>
2023-11-30 15:52:24 -0600

    commit
    primer commit

---
+++ include/chip8.h
@@ -0,0 +1,40 @@
+#ifndef CHIP8_T_H
+#define CHIP8_T_H
+
+#include <stdint.h>
+
+struct chip8
+{
+    //Memoria ram	
+    char RAM[4096];
+    
+    //Registros
+    uint8_t V[16];
+    uint16_t PC;
+    uint8_t SP;
+    uint16_t I;
+    uint16_t Pila[16];
+    
+    //temporizadores
+    uint8_t temp_delay;
+    uint8_t temp_sonido;
+    
+    //teclas - controles
+    //
+
+    //pantalla
+    unsigned int pantalla[64 * 32];
+    
+    //opcodes - instrucciones
+    uint16_t opcode;
+};
+
+//funciones de inicializar, reiniciar, cicloFDE, lectura de roms, timers, graficos...
+void ini_cmptes(struct chip8 *c8);
+void cicloFDE(struct chip8 *c8);
+//funciones de reinicio, entrada del teclado, lectura de roms, timers..
+//
+//
+//
+//
+#endif
---
+++ src/chip8.c
@@ -0,0 +1,113 @@
+#include "../include/chip8.h"
+
+void ini_cmptes(struct chip8 *c8)
+{
+    //Limpiar memoria.
+
+    for(int i = 0; i < 4096; i++)
+    {
+        c8->RAM[i] = 0;
+    }
+
+    //Limpiar registros y pila.
+
+    for(int i = 0; i < 16; i++)
+    {
+        c8->V[i] = 0;
+	c8->Pila[i] = 0;
+    }
+
+    c8->PC = 0x200;
+    c8->SP = 0;
+    c8->I = 0;
+
+    //Limpiar pantalla;
+    
+    for(int i = 0; i < 62*32; i++)
+    {
+        c8->pantalla[i] = 0;
+    }
+
+    //Cargar fuentes.
+    //
+    // 
+
+    //Limpiar timers.
+
+    c8->temp_delay = 0;
+    c8->temp_sonido = 0;
+
+    //Limpiar entrada.
+    //
+    //
+    c8->opcode = 0;
+}
+
+void cicloFDE(struct chip8 *c8)
+{
+     //Variables para guardar partes especificas que vienen en el opcode
+     //esto es registros y direcciones de 12 y 8 bits.	
+     uint16_t nnn = 0; //direcion de 12 bits
+     uint8_t nn = 0; //direccion de 8 bits
+     uint8_t reg_x = 0; //registro X
+     uint8_t reg_y = 0; //registro Y    
+
+     //Obtenemos el opcode. (Fetch)
+     c8->opcode = c8->RAM[c8->PC] << 8 | c8->RAM[c8->PC + 1];	
+     
+     //Decodificar opcode. (Decode)
+
+     switch(c8->opcode & 0xF000)
+     {
+         case 0x0000: //Ejecutar instruccion (Execute) (Para el grupo de instrucciones 0x0000)
+   	     switch(c8->opcode & 0x00FF)
+	     {
+	         case 0x00E0: //Limpiar pantalla.
+			 for(int i = 0; i < 64*32; i++)
+			 {
+			     c8->pantalla[i] = 0;
+			 }
+		 break;
+		 case 0x00EE: //Retorno de un subrutina.
+		         c8->SP--; //se decrementa el SP en 1.
+			 //En PC se guarda la direccion a la que apunta SP en la pila en ese momento.	   
+			 c8->PC = c8->Pila[c8->SP]; 
+			 //Al ocurrir eso el PC se incrementa en dos.
+			 c8->PC += 2;
+		 break;
+	     }
+	 break;
+	 case 0x1000: //Intruccion de salto. Salta a la direccion 'nnn' es decir, una direccion de 12 bits.
+	     nnn = c8->opcode & 0x0FFF; //obtenemos dicha direccion incrustada en el opcode
+	     
+	     c8->PC = nnn; //establecemos el PC con dicha direccion
+	 break;
+	 case 0x2000:
+	     c8->Pila[c8->SP] = c8->SP;
+	     c8->SP++;
+
+	     nnn = c8->opcode & 0x0FFF;
+
+	     c8->PC = nnn;
+	 break;
+	 case 0x3000: //Salta a la siguiente instruccion si el registro Vx es igual a la direccion nn (!)
+             //obtenemos el registro en cuestion
+     	     reg_x = (c8->opcode & 0x0F00) >> 8;
+	     nn = c8->opcode & 0x00FF; //obtenemos la direccion nn.
+
+	     if(c8->V[reg_x] == nn) //hacemos la comparacion.
+	     {
+	        c8->PC += 2;
+	     }
+	 break;
+	 case 0x4000: //Salta a la siguiente instruccion si el registrr Vx es diferente de la direccion nn 
+	     reg_x = (c8->opcode & 0x0F00) >> 8;
+	     nn = c8->opcode & 0x00FF;
+
+	     if(c8->V[reg_x] != nn)
+	     {
+		 c8->PC += 2;    
+	     }
+	 break;
+     }
+} 
