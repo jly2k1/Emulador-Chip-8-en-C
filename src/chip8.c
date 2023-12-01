76d1d0f41b4218265e30dfd4f7450abf12bff6da
jly2k1 <jaimelobar@gmail.com>
2023-11-30 20:05:26 -0600

    implementacion del opcode 5xy0 al fx07.
    Se implementaron los demas opcodes a partir del 5xy0 al fx07. No se programaron los opcodes correspodientes al dibujado de sprites ni relacionados a los controles.

--- src/chip8.c
+++ src/chip8.c
@@ -1,4 +1,5 @@
 #include "../include/chip8.h"
+#include <stdlib.h>
 
 void ini_cmptes(struct chip8 *c8)
 {
@@ -129,5 +130,163 @@ void cicloFDE(struct chip8 *c8)
 	     nn = c8->opcode & 0x00FF;
 
 	     c8->V[reg_x] += nn;
+	 break;
+	 case 0x8000:
+	     switch(c8->opcode & 0x000F)
+	     {
+	         case 0x0000: //Agrega el valor del registro Y al registro X.
+			 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			 c8->V[reg_x] = c8->V[reg_y];
+		 break;
+		 case 0x0001: //Realiza la operacion OR entre los registros X e Y, guarda el resultado en X.
+		 	 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			 c8->V[reg_x] |= c8->V[reg_y];
+		 break;
+		 case 0x0002: //Realiza la operacion AND entre los registros X e Y, guarda el resultado en X.
+		 	 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			 c8->V[reg_x] &= c8->V[reg_y];
+		 break;
+		 case 0x0003: //Realiza la operacion XOR entre los registros X e Y, guarda el resultado en X.
+		 	 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			c8->V[reg_x] ^= c8->V[reg_y]; 
+		 break;
+		 /*Realiza la suma de los registros X e Y, guarda el resultado en X y si hay un acarreo,
+		  * establece el registro F en 1. En caso de que no se de el acarreo, F se establece en
+		  * 0.*/
+		 case 0x0004: //(!)
+			 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			 c8->V[reg_x] += c8->V[reg_y];
+
+			 /*Para saber si hay un acarreo, hay que comprobar si se supero el valor máximo
+			  * que pueden guardar nuestros registros de 8 bits. Eso es, comprobar si su
+			  * suma supera el valor 255*/
+			 if(c8->V[reg_x] + c8->V[reg_y] > 255)
+			 {
+			     c8->V[0xF] = 1;
+			 }
+			 else
+			 {
+			     c8->V[0xF] = 0;
+			 }
+		   break;
+		   /*Realiza la resta de los registros X e Y, guarda el resultado en X y si hubo un 'prestamo'
+		    * establece el registro de F en 0. En caso de que no se diese un prestamo, F se establece
+		    * en 1.*/
+		   case 0x0005:
+		   	 reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+		 	 /*Para saber si va haber un prestamo, comprobamos si el valor del registro X
+			  * es mayor al del Y. En caso de serlo no habrá prestamo y en caso contrario
+			  * si lo hará.*/
+			 if(c8->V[reg_x] > c8->V[reg_y]) //(!)
+			 {
+			     c8->V[0xF] = 1;
+			 }
+			 else
+			 {
+			     c8->V[0xF] = 0;
+			 }
+
+			 c8->V[reg_x] -= c8->V[reg_y];
+		   break;
+		   /*Guarda el bit menos significativo del registro X en el registro F y desplaza un 1 bit
+		    * hacia la derecha el registro X.*/
+		   case 0x0006: 
+		         reg_x = (c8->opcode & 0x0F00) >> 8;
+			 
+			 //obtenemos el dicho bit utilizando el operador '&' y el valor 0x1.
+			 c8->V[0xF] = c8->V[reg_x] & 0x1;
+			 
+			 c8->V[reg_x] >>= 1; //hacemos el desplazamiento
+		   break;
+		   /*Comprueba si el registro Y es mayor al registro X. En caso de serlo el registro
+		    * F = 1 y en caso de no serlo F = 0. Se resta el registro Y con X y se guarda
+		    * el resultado en X.*/
+		   case 0x0007:
+		         reg_x = (c8->opcode & 0x0F00) >> 8;
+			 reg_y = (c8->opcode & 0x00F0) >> 4;
+
+			 if(c8->V[reg_y] > c8->V[reg_x])
+			 {
+			     c8->V[0xF] = 1;
+			 }
+			 else
+			 {
+			     c8->V[0xF] = 0;
+			 }
+
+			 c8->V[reg_x] = c8->V[reg_y] - c8->V[reg_x];
+		   break;
+		   /*Esta instruccion es similar a 8XY6, solo que aqui obtenemos el bit mas significativo
+		    * de X y lo guardamos en F*/
+		   case 0x000E:
+		         reg_x = (c8->opcode & 0x0F00) >> 8;
+			 
+			 //obtenemos el bit mas significativo
+			 c8->V[0xF] = (c8->V[reg_x] & 0x80) >> 7;
+                         
+			 //desplazamos una posicion hacia la izquierda.
+			 c8->V[reg_x] <<= 1;
+		   break;
+	     }
+	 case 0x9000: //Salta a la siguiente instruccion si el registro X es diferente de Y.
+	     reg_x = (c8->opcode & 0x0F00) >> 8;
+	     reg_y = (c8->opcode & 0x00F0) >> 4;
+
+	     if(c8->V[reg_x] != c8->V[reg_y])
+	     {
+	         c8->PC += 2;
+	     }
+	 break;
+	 case 0xA000: //Establece en el registro indice la direccion 'nnn'.
+	     nnn = c8->opcode & 0x0FFF;
+
+	     c8->I = nnn;
+	 break;
+	 case 0xB000: //Salta a la ubicacion resultante de la suma del registro 0 mas la direccion nnn.
+	     nnn = c8->opcode & 0x0FFF;
+
+	     c8->PC = c8->V[0x0] + nnn;
+	 break; //Se genera un numero aleatorio y con un '&' con la direccion 'nn' y el resultado se guarda en X
+	     uint8_t valor_aleatorio = rand() + 256;
+	     
+	     reg_x = (c8->opcode & 0x0F00) >> 8;
+	     nn = c8->opcode & 0x00FF;
+
+	     c8->V[reg_x] = valor_aleatorio & nn;
+	 break;
+	 case 0xD000:
+	 //instruccion que pinta un sprite en la pantalla.
+	 break;
+	 case 0xE000:
+	     switch(c8->opcode & 0x000F)
+	     {
+	 	    case 0x000E:
+			  //saltar a la siguiente instruccion si X coincide con la tecla presionada.
+	 	    break;
+		    case 0x0001:
+		         //saltar a la siguiente instruccion si X no coincide con la tecla presionada.
+		    break;
+	     }
+	 case 0xF000:
+	     switch(c8->opcode & 0x000F)
+	     {
+	     	    case 0x0007:
+			  reg_x = (c8->opcode & 0x0F00) >> 8;
+			  
+			  c8->V[reg_x] = c8->temp_delay;
+		    break;	  
+	     } 
      }
 } 
